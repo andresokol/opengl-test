a18abf5c27a731810c0dffbdc5d1b2dcd4c11e54
Andrey Sokolov <sokolov-as-i@yandex.ru>
2016-11-16 13:54:43 +0300

    hello window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,60 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+
 #include <iostream>
-//#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Init GLFW
+    glfwInit();
+
+    // Set up GLFW
+    // Set minimal required OpenGL version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Set up profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Make window resizable
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // Create window object
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Init GLEW
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // Passing window size to OpenGL
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+
+    // Game loop while no 'close window' signal recieved
+    while(!glfwWindowShouldClose(window))
+    {
+        // check for events (eg key pressed)
+        glfwPollEvents();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    // release resources
+    glfwTerminate();
+
     return 0;
 }
